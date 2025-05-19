87780ba8fa0b4576d1221a55919cadaf36309953
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-19 10:06:07 +0200

    barebones version

--- backend/rust_benchmark/src/main.rs
+++ backend/rust_benchmark/src/main.rs
@@ -1,8 +1,5 @@
-use tesseract::Tesseract;
 use pdfium_render::prelude::*;
 use std::env;
-use std::fs;
-use std::path::Path;
 
 fn main() {
     // Argument: path naar PDF-bestand
@@ -16,20 +13,6 @@ fn main() {
     let pdfium = Pdfium::new(Pdfium::bind_to_system_library().unwrap());
 
     let doc = pdfium.load_pdf_from_file(pdf_path, None).unwrap();
-    let mut ocr_engine = LepTess::new(None, "eng").unwrap();
-
-    for (i, page) in doc.pages().iter().enumerate() {
-        let bitmap = page.render().render().unwrap();
-
-        let png_path = format!("page_{}.png", i + 1);
-        bitmap.as_image().save(&png_path).unwrap();
-
-        ocr_engine.set_image(&png_path);
-        let text = ocr_engine.get_utf8_text().unwrap();
-
-        println!("--- Pagina {} ---\n{}", i + 1, text);
-        fs::remove_file(png_path).unwrap();
-    }
 }
 
 
