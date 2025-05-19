efd1e3a3d13606065b17c39726e862839ef35473
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-19 10:27:14 +0200

    simple code

--- backend/rust_benchmark/src/main.rs
+++ backend/rust_benchmark/src/main.rs
@@ -1,18 +1,6 @@
-use pdfium_render::prelude::*;
-use std::env;
+use lopdf::{Document};
 
 fn main() {
-    // Argument: path naar PDF-bestand
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        eprintln!("Gebruik: ocr_service <pad/naar/bestand.pdf>");
-        std::process::exit(1);
-    }
-
-    let pdf_path = &args[1];
-    let pdfium = Pdfium::new(Pdfium::bind_to_system_library().unwrap());
-
-    let doc = pdfium.load_pdf_from_file(pdf_path, None).unwrap();
+    let path = "../../files/DIGI_2007_000118_01.pdf"; 
+    let doc = Document::load(path).expect("Failed to open PDF file");
 }
-
-
