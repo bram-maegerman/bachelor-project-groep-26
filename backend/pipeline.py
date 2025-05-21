a8d30b58277698a973b0206d88279e09d4ed854f
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-21 10:37:32 +0200

    refactored pipeline to handle more then one pdf

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,10 +1,11 @@
 import os, sys
 
-if len(sys.argv) != 2:
-    print("Error: pipeline.py expects 1 argument")
+if len(sys.argv) < 2:
+    print("Error: pipeline.py expects at least one argument")
     sys.exit(1)
 
-pdf_path = sys.argv[1]
+pdf_paths = sys.argv[1:]
 
-# TODO: change this to use the rust script instead of test.py
-os.system(f"py test.py {pdf_path} | py flagger.py")
+for pdf_path in pdf_paths:
+    # TODO: change this to use the rust script instead of test.py
+    os.system(f"py test.py {pdf_path} | py flagger.py")
