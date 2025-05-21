4756b7e6e909dcfe512d4e203988b4a73f1c3cf6
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-21 11:04:26 +0200

    using python file picker to extract absolute paths

--- frontend/main.py
+++ frontend/main.py
@@ -4,6 +4,12 @@ class API:
     def greet(self):
         return "Hello from backend!"
 
+    def open_file_dialog(self):
+        # Use the window created by webview
+        window = webview.windows[0]
+        result = window.create_file_dialog(webview.OPEN_DIALOG)
+        return result[0] if result else None
+        
 api = API()
-webview.create_window("Scan-Checker", "gui/index.html", js_api=api)
+webview.create_window("Scan-Checker", "gui/test.html", js_api=api)
 webview.start(debug=True)
