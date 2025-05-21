375810ffcde04f9e097c6132763c4a4edeb95b1f
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-21 09:41:01 +0200

    pipeline now accepts path to one pdf

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -1,3 +1,10 @@
-import os
+import os, sys
+
+if len(sys.argv) != 2:
+    print("Error: pipeline.py expects 1 argument")
+    sys.exit(1)
+
+pdf_path = sys.argv[1]
+
 # TODO: change this to use the rust script instead of test.py
-os.system("py test.py | py flagger.py")
+os.system(f"py test.py {pdf_path} | py flagger.py")
