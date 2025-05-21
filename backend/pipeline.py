1f867a579fc436277eb1219da1113378af9c6967
bram-maegerman <91273521+bram-maegerman@users.noreply.github.com>
2025-05-21 14:27:50 +0200

    changed pipeline to be called from the frontend

--- backend/pipeline.py
+++ backend/pipeline.py
@@ -8,4 +8,4 @@ pdf_paths = sys.argv[1:]
 
 for pdf_path in pdf_paths:
     # TODO: change this to use the rust script instead of test.py
-    os.system(f"py test.py {pdf_path} | py flagger.py")
+    os.system(f"py ../backend/test.py {pdf_path} | py ../backend/flagger.py")
