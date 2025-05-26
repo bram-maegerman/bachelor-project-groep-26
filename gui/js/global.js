e5e8693a5bb7c79c7fdb03d06451e184d155ebbe
DavyBellens <r0882857@ucll.be>
2025-05-26 23:22:36 +0200

    garbage navbar (v1)

--- gui/js/global.js
+++ gui/js/global.js
@@ -1,5 +1,22 @@
-// File voor globale functies die in alle pagina's gebruikt kunnen worden
+const ARROW_UP = "▲"; // \u25B2
+const ARROW_DOWN = "▼"; // \u25BC
 
-back.addEventListener("click", () => {
-  window.history.back();
+function toggleArrow(currentArrow) {
+  return currentArrow === ARROW_UP ? ARROW_DOWN : ARROW_UP;
+}
+
+const navDropdown = document.getElementById("nav-dropdown");
+const navArrow = document.getElementById("nav-arrow");
+const options = document.getElementById("options");
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (navDropdown && navArrow && options) {
+    navDropdown.addEventListener("click", () => {
+      navArrow.textContent = toggleArrow(navArrow.textContent.trim());
+      options.classList.toggle("hidden");
+      navDropdown.style.borderRadius = options.classList.contains("hidden")
+        ? "25px"
+        : "25px 25px 0 0";
+    });
+  }
 });
